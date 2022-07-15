4e5c83f3618129057f62ea96d204fa31f956e742
Bhaskarateja7 <bhaskerteja.boddu@intellisavvy.com>
2022-07-15 11:58:59 +0530

    Added database connection

---
+++ config/db.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const pool = mysql.createPool({
+connectionLimit : 100,
+host : process.env.HOST,
+user : process.env.USER,
+password: process.env.PASSWORD,
+database : process.env.DATABASE
+
+});
+
+module.exports = pool ; 
